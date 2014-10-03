99921faeb789b3f95a7768158f2bc430386e5f26
Abu Saleh Muhammad Shaon <srabon.php@gmail.com>
2014-10-03 17:29:47 +0600

    colors loop added

--- index.php
+++ index.php
@@ -21,6 +21,8 @@ $mainArray = array(
 	),
 );
 
+$colors = array('#00DDFF', '#00FF00', '#FF0000', '#00FF33', '#11CCEE');
+
 $headerTexts = array_keys($mainArray[0]);
 ?>
 
@@ -29,7 +31,7 @@ $headerTexts = array_keys($mainArray[0]);
 <table class="table">
 	<tr>
 		<?php for ($i = 0; $i < count($headerTexts); $i++): ?>
-			<th width="<?php echo 100/(count($headerTexts)); ?>%">
+			<th style="background-color: <?php echo $colors[$i] ?>" width="<?php echo 100/(count($headerTexts)); ?>%">
 				<?php echo $headerTexts[$i]; ?>
 			</th>
 		<?php endfor; ?>
@@ -37,7 +39,7 @@ $headerTexts = array_keys($mainArray[0]);
 	<?php foreach ($mainArray as $k1 => $v1): ?>
 		<tr>
 			<?php for ($i = 0; $i < count($headerTexts); $i++): ?>
-				<td>
+				<td style="background-color: <?php echo $colors[$i]; ?>">
 					<?php echo $v1[$headerTexts[$i]]; ?>
 				</td>
 			<?php endfor; ?>
